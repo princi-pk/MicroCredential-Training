d953682a8d3d5ee1ca4295cd7f353da88333bddb
princi-pk <75043174+princi-pk@users.noreply.github.com>
2021-04-28 22:41:50 +0530

    Update SwaggerConfig.java

--- Milestone1/loginRegistration/src/main/java/com/bankmanagementsystem/loginRegistration/configuration/SwaggerConfig.java
+++ Milestone1/loginRegistration/src/main/java/com/bankmanagementsystem/loginRegistration/configuration/SwaggerConfig.java
@@ -17,7 +17,7 @@ public class SwaggerConfig {
 	@Bean
 	public Docket swaggerConfiguration() {
 		return new Docket(DocumentationType.SWAGGER_2).select()
-				.apis(RequestHandlerSelectors.basePackage("com.bankmanagemtsystem.customerRegistration")).build()
+				.apis(RequestHandlerSelectors.basePackage("com.bankmanagementsystem.loginRegistration")).build()
 				.apiInfo(apiInfo());
 	}
 
